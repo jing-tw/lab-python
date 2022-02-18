67c66c95950235ccdc4db648a880d5dac2128627
jing-tw <mqjing@gmail.com>
2022-02-18 16:32:58 +0800

    [add] add more plot code

---
+++ lab/02-signal-process/01-psd/README.md
@@ -0,0 +1,10 @@
+https://docs.google.com/document/d/1bDrt208Sedhiqx-GdtyJcNvVdsO1OtNwmBnDNjK98Io/edit?usp=sharing
+
+Mermaid Test
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+
--- lab/02-signal-process/01-psd/org_fft_vs_psd.py
+++ lab/02-signal-process/01-psd/org_fft_vs_psd.py
@@ -4,16 +4,16 @@ from numpy import where
 from numpy.fft import fft
 
 
-N = 1600         # Define the total number of data points 
-fs = 800.0        # Define the sampling rate
+N = 10*10000         # Define the total number of data points 
+fs = 400.0        # Define the sampling rate
 dt = 1.0 / fs      # Define the sampling interval (sec)
 T = N*dt         # Define the total duration of data (sec)
 t = np.linspace(0.0, N*dt, N) # start=0, stop = N*T, total sample = N
 s = np.sin(50.0 * 2.0*np.pi*t) + 0.5*np.sin(80.0 * 2.0*np.pi*t)
 
 # Calcuating the Spectrum
-# sf = fft(s) 
-sf = fft(s-s.mean())  # calcuate the fft
+sf = fft(s) 
+# sf = fft(s-s.mean())  # calcuate the fft
 
 # Calcuating the Power Spectrum
 # page: https://mark-kramer.github.io/Case-Studies-Python/03.html
@@ -29,11 +29,15 @@ Sxx = (sf * sf.conj()) * 1.0/2 * 0.5*fs/T
 # Sxx[2:len(Sxx)-1] = 2*Sxx[2:len(Sxx)-1] # matlab
 # Sxx = 10 * np.log10(Sxx) # matlab
 
+
+left = 49.5
+right = 50.5
 # Show the signal
 fig, (ax0, ax1, ax2) = plt.subplots(3, 1)
 ax0.set_title('Signal ' +  'T = ' + str(T) + ' (sec)')
 ax0.set_xlabel('Time [s]')
 ax0.set_ylabel('Voltage [$\mu V$]')
+ax0.set_xlim(0, 1)
 ax0.plot(t, s)
 
 # Determine frequency resolution (df = fNQ / (N//2) ???)
@@ -41,18 +45,27 @@ fNQ = fs/ 2.0                       # Determine Nyquist frequency
 faxis = np.linspace(0.0, fNQ, N//2) # Construct frequency axis
 
 # Show the spectrum
-ax1.set_title('FFT')
+
+max_y_fft = np.abs(sf[:N//2]).max();
+
+ax1.set_title('FFT ' + ' max_y_fft = ' + str(max_y_fft))
 ax1.set_xlabel('Frequency [Hz]')
 ax1.set_ylabel('Magnitude')
-ax1.set_xlim(0,100)
+ax1.set_xlim(left, right)
+# ax1.set_ylim(max_y_fft - 100, max_y_fft)
+ax1.set_ylim(0, 100000)
 ax1.plot(faxis, np.abs(sf[:N//2]))   # Ignore negative frequencies
 # ax1.plot(faxis, 2.0/N * np.abs(sf[:N//2])) // amplitude normalization
 
 # Show the power spectrum
-ax2.set_title('PSD')
+max_y_psd = np.abs(Sxx[:N//2]).max();
+
+ax2.set_title('PSD ' + ' max_y_psd = ' + str(max_y_psd))
 ax2.set_xlabel('Frequency [Hz]')
 ax2.set_ylabel('Power [$\mu V^2$/Hz]')
-ax2.set_xlim(0,100)
+ax2.set_xlim(left, right)
+# ax2.set_ylim(max_y_psd - 100, max_y_psd)
+ax2.set_ylim(0, 100000)
 ax2.plot(faxis, np.abs(Sxx[:N//2]))  # Ignore negative frequencies
 # end of test
 fig.tight_layout()
