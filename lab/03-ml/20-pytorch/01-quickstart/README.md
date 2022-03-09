b0ae9922d7db97c3a8f911e68d60c92797a0a9e1
jing <mqing@gmail.com>
2022-03-09 15:42:00 +0800

    [add] classifer for FashionMNIST

---
+++ lab/03-ml/20-pytorch/01-quickstart/README.md
@@ -0,0 +1,7 @@
+# Run
+```bash
+python test.py
+```
+
+# Reference
+1. https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
--- lab/03-ml/20-pytorch/01-quickstart/src/test.py
+++ lab/03-ml/20-pytorch/01-quickstart/src/test.py
@@ -5,25 +5,130 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
 # Download training/testing data from open datasets.
 def download_data():
     training_data = datasets.FashionMNIST(
-        root='data',
-        train=True,
-        download=True,
-        transform=ToTensor,
+        root = "data",
+        train = True,
+        download = True,
+        transform = ToTensor(),
     )
 
     test_data = datasets.FashionMNIST(
-        root='data',
-        train=False,
-        download=True,
-        transform=ToTensor(),
+        root = "data",
+        train = False,
+        download = True,
+        transform = ToTensor(),
     )
 
+    return training_data, test_data
+
+def create_data_loader(training_data, test_data):
+    batch_size = 64
+
+    #create data loader
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+
+    return train_dataloader, test_dataloader
+
+def create_model():
+    # Get cpu or gpu device for training.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    return model, device
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 def main():
     print(torch.__version__)
-    download_data()
+    training_data, test_data = download_data()
+    train_dataloader, test_dataloader = create_data_loader(training_data, test_data)
 
-main()
+    model, device = create_model()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
+
+
+import time
+
+class Timer:    
+    def __enter__(self):
+        self.start = time.process_time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.process_time()
+        self.interval = self.end - self.start
+
+with Timer() as t:
+    print('Your long run procedure')
+    main()
+print('Request took %.03f sec.' % t.interval)
