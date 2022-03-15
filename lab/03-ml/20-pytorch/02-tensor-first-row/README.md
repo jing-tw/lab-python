13e8f81e069bb732bd34dbb4b0b5a26866c9f96f
Jing <mqjing@gmail.com>
2022-03-15 00:27:46 -0700

    [add] Get the first row of tensor

---
+++ lab/03-ml/20-pytorch/02-tensor-first-row/README.md
@@ -0,0 +1,4 @@
+# Show the first row of tensor
+
+# Google doc
+https://docs.google.com/document/d/1Knrp4di54nda443mIHJ7eB0eLwWa6fb4y4qhgyc4UeM/edit?usp=sharing
---
+++ lab/03-ml/20-pytorch/02-tensor-first-row/src/test.py
@@ -0,0 +1,7 @@
+import torch
+
+data = [[1, 2], [3, 4]]
+tensor = torch.tensor(data)
+print(f"tensor = {tensor}")
+print(f"First row: {tensor[0]}")
+print(f"Second row: {tensor[1]}")
